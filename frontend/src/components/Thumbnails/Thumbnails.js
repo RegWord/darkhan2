c4bf5b3cde191a78c52229d7a5b97ea04248c451
RegWord <fernandezz228@mail.ru>
2025-02-11 05:59:21 +0500

    13

--- frontend/src/components/Thumbnails/Thumbnails.js
+++ frontend/src/components/Thumbnails/Thumbnails.js
@@ -3,48 +3,60 @@ import { Link } from 'react-router-dom';
 import Price from '../Price/Price';
 import StarRating from '../StarRating/StarRating';
 import classes from './thumbnails.module.css';
-export default function Thumbnails({ foods }) {
+
+export default function Thumbnails({ foods = [] }) {
+  console.log("Received foods:", foods); // Проверяем, что приходит
+
+  if (!Array.isArray(foods)) {
+    return <p>Ошибка: данные не загружены</p>; // Безопасный рендер
+  }
+
   return (
     <ul className={classes.list}>
-      {foods.map(food => (
-        <li key={food.id}>
-          <Link to={`/food/${food.id}`}>
-            <img
-              className={classes.image}
-              src={`${food.imageUrl}`}
-              alt={food.name}
-            />
-
-            <div className={classes.content}>
-              <div className={classes.name}>{food.name}</div>
-              <span
-                className={`${classes.favorite} ${
-                  food.favorite ? '' : classes.not
-                }`}
-              >
-                ❤
-              </span>
-              <div className={classes.stars}>
-                <StarRating stars={food.stars} />
-              </div>
-              <div className={classes.product_item_footer}>
-                <div className={classes.origins}>
-                  {food.origins.map(origin => (
-                    <span key={origin}>{origin}</span>
-                  ))}
+      {foods.length > 0 ? (
+        foods.map(food => (
+          <li key={food.id}>
+            <Link to={`/food/${food.id}`}>
+              <img
+                className={classes.image}
+                src={`${food.imageUrl}`}
+                alt={food.name}
+              />
+              <div className={classes.content}>
+                <div className={classes.name}>{food.name}</div>
+                <span
+                  className={`${classes.favorite} ${food.favorite ? '' : classes.not}`}
+                >
+                  ❤
+                </span>
+                <div className={classes.stars}>
+                  <StarRating stars={food.stars} />
                 </div>
-                <div className={classes.cook_time}>
-                  <span>🕒</span>
-                  {food.cookTime}
+                <div className={classes.product_item_footer}>
+                  <div className={classes.origins}>
+                    {Array.isArray(food.origins) ? (
+                      food.origins.map(origin => (
+                        <span key={origin}>{origin}</span>
+                      ))
+                    ) : (
+                      <span>Нет данных</span>
+                    )}
+                  </div>
+                  <div className={classes.cook_time}>
+                    <span>🕒</span>
+                    {food.cookTime}
+                  </div>
+                </div>
+                <div className={classes.price}>
+                  <Price price={food.price} />
                 </div>
               </div>
-              <div className={classes.price}>
-                <Price price={food.price} />
-              </div>
-            </div>
-          </Link>
-        </li>
-      ))}
+            </Link>
+          </li>
+        ))
+      ) : (
+        <p>Нет доступных блюд</p>
+      )}
     </ul>
   );
 }
